347ecec0914a9e0d76c9ca0c8df3aae2b55e0cac
LakesideMiners <lgarvic@icloud.com>
2020-12-15 11:30:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import praw
 import requests
 import os
-reddit = praw.Reddit(client_id=os.getenv('client_id'), client_secret=os.getenv('client_secret'), user_agent=os.getenv('user_agent') )
+reddit = praw.Reddit(client_id=os.environ['client_id'], client_secret=os.environ['client_secret'], user_agent=os.environ['user_agent'] )
 for submission in reddit.subreddit("furry_irl").new(limit=20):
   yiff = submission.over_18
   if yiff == False:
@@ -9,8 +9,8 @@ for submission in reddit.subreddit("furry_irl").new(limit=20):
     title= submission.title
     break
 def telegram_bot_sendtext(bot_message):
-    bot_token = os.getenv('bot_api')
-    bot_chatID = os.getenv('chat_id')
+    bot_token = os.environ['bot_api']
+    bot_chatID = os.environ['chat_id']
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
     response = requests.get(send_text)
     return response.json()
